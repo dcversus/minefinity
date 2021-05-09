28c41aa0e0574bec1f5b50e87c28f0055c3e95e1
Vasilisa Versus <versus@fintuity.com>
2021-05-10 00:32:47 +0200

    wip reveal and chunks

--- src/index.ts
+++ src/index.ts
@@ -15,49 +15,103 @@ import flag from './assets/flag.png';
 import wrong from './assets/wrong.png';
 import open from './assets/open.png';
 
-// document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+const LOAD_RADIUS = 32;
+
 let textures = {} as any;
-const WIDTH = 16;
+const TILE_WIDTH = 16;
+const FIELD_WIDTH = 10000;
+const FIELD_HEIGHT = 10000;
+const FIELD_MINES = 100000000;
+
 let cursor = [0,0];
-const field = {
-  ['x_y']: undefined,
-}
+const field = {}
 
 enum STATE {
   empty,
   flagged,
   mine,
   wrong,
-  one,
-  two,
-  three,
-  four,
-  five,
-  six,
-  seven,
-  eight
+  tip,
+  closed
 }
 
-function click([x, y], stt = false) {
+function isCellAdded([x, y]: [number, number]): boolean {
+  let cell = field[`${x}_${y}`];
+  
+  return !!cell;
+}
+
+function getCell([x, y]: [number, number]): Cell {
   let cell = field[`${x}_${y}`];
 
   if (! cell) {
-    cell = field[`${x}_${y}`] = new Cell(x, y, stt ? STATE.flagged : STATE.empty);
-    gameContainer.addChild(cell);
-  } else {
-    cell.update(STATE.mine);
+    cell = field[`${x}_${y}`] = new Cell(x, y);
+    // if (Math.pow(x - cursor[0], 2) + Math.pow(y - cursor[1], 2) < Math.pow(LOAD_RADIUS, 2)) {
+    //   gameContainer.addChild(cell);
+    // }
+  }
+
+  return cell;
+}
+
+
+function sleep(dur, val) {
+  return new Promise(res => {
+    setTimeout(() => { res(val) }, dur);
+  });
+}
+
+function* getNeighbours([x, y], findAllEmpty) {
+  const offsetRange = [-1,0,1];
+
+  for (let xOffset of offsetRange) {
+    const xNeighbour = x - xOffset;
+
+    if (xNeighbour >= 0 && xNeighbour < FIELD_WIDTH) {
+      for (let yOffset of offsetRange) {
+        const yNeighbour = y - yOffset;
+
+        if (yNeighbour >= 0 && yNeighbour < FIELD_HEIGHT) {
+          if(yOffset === 0 && xOffset === 0) {
+            continue;
+          }
+
+          const isNotAdded = !isCellAdded([xNeighbour, yNeighbour]);
+          const cell = getCell([xNeighbour, yNeighbour]);
+
+          // if (findAllEmpty) {
+          //   yield sleep(1, cell);
+          // } else {
+            yield cell;
+          // }
+
+          if (findAllEmpty && isNotAdded) {
+            yield* getNeighbours([xNeighbour, yNeighbour], true);
+          }
+        }
+      }
+    }
   }
 }
 
 class Cell extends PIXI.Container {
-  state: STATE;
+  state = STATE.closed;
+  neighbours = 0;
+  isMine = false;
+  isOpen = false;
+  isFlagged = false;
+  cellX: number;
+  cellY: number;
 
-	constructor(x: number, y: number, state: STATE){
+	constructor(x: number, y: number) {
 		super();
-		this.x = x * WIDTH;
-    this.y = y * WIDTH;
-    this.state = state;
-    let [ backTexture, frontTexture ] = this.getTexture(state);
+		this.x = x * TILE_WIDTH;
+    this.y = y * TILE_WIDTH;
+		this.cellX = x;
+    this.cellY = y;
+    let [ backTexture, frontTexture ] = this.getTexture();
 
 		let back = new PIXI.Sprite(backTexture);
 		let front = new PIXI.Sprite(frontTexture);
@@ -65,8 +119,53 @@ class Cell extends PIXI.Container {
 		this.addChildAt(front, 1);
   }
 
-  update(state: STATE) {
-    let [ backTexture, frontTexture ] = this.getTexture(state);
+  addNeighbour() {
+    this.neighbours++;
+  }
+
+  placeMine() {
+    this.isMine = true;
+
+    return this;
+  }
+
+  open() {
+    if (this.isFlagged) {
+      return;
+    }
+
+    if (this.neighbours) {
+      this.state = STATE.tip;
+    } else {
+      this.state = STATE.empty;
+    }
+
+    if (this.isMine) {
+      this.state = STATE.wrong;
+    }
+
+    this.isOpen = true;
+
+    this.updateView();
+  }
+
+  get isSafeForOpen() {
+    return !this.isMine && !this.isOpen && !this.isFlagged;
+  }
+
+  markFlagged() {
+    if (this.isOpen) {
+      return;
+    }
+
+    this.isFlagged = ! this.isFlagged;
+
+    this.state = this.isFlagged ? STATE.flagged : STATE.closed;
+    this.updateView();
+  }
+
+  updateView() {
+    let [ backTexture, frontTexture ] = this.getTexture();
 
 		let back = this.getChildAt(0) as PIXI.Sprite;
 		let front = this.getChildAt(1) as PIXI.Sprite;
@@ -74,53 +173,30 @@ class Cell extends PIXI.Container {
 		front.texture = frontTexture;
   }
 
-  getTexture(state) {
-    if (state === STATE.empty) {
+  getTexture() {
+    if (this.state === STATE.closed) {
+      return [textures.closed.texture, PIXI.Texture.EMPTY];
+    }
+
+    if (this.state === STATE.empty) {
       return [textures.open.texture, PIXI.Texture.EMPTY];
     }
 
-    if (state === STATE.flagged) {
+    if (this.state === STATE.tip) {
+      const tipNumber = this.neighbours.toString();
+      return [textures.open.texture, textures[tipNumber].texture];
+    }
+
+    if (this.state === STATE.flagged) {
       return [textures.closed.texture, textures.flag.texture];
     }
 
-    if (state === STATE.mine) {
+    if (this.state === STATE.mine) {
       return [textures.open.texture, textures.mine.texture];
     }
 
-    if (state === STATE.wrong) {
+    if (this.state === STATE.wrong) {
       return [textures.open.texture, textures.wrong.texture];
-    }
-
-    if (state === STATE.one) {
-      return [textures.open.texture, textures.one.texture];
-    }
-
-    if (state === STATE.two) {
-      return [textures.open.texture, textures.two.texture];
-    }
-
-    if (state === STATE.three) {
-      return [textures.open.texture, textures.three.texture];
-    }
-
-    if (state === STATE.four) {
-      return [textures.open.texture, textures.four.texture];
-    }
-
-    if (state === STATE.five) {
-      return [textures.open.texture, textures.five.texture];
-    }
-
-    if (state === STATE.six) {
-      return [textures.open.texture, textures.six.texture];
-    }
-
-    if (state === STATE.seven) {
-      return [textures.open.texture, textures.seven.texture];
-    }
-
-    if (state === STATE.eight) {
-      return [textures.open.texture, textures.eight.texture];
     }
   }
 }
@@ -157,12 +233,9 @@ new PIXI.Loader()
   .load(setup);
 
 
-function setup(loader, resources) {
-  const mines = 10;
-  const width = 2147483647 ;
-  const height = 2147483647 ;
-  const containerWidth = width * WIDTH;
-  const containerHeight = height * WIDTH;
+async function setup(loader, resources) {
+  const containerWidth = FIELD_WIDTH * TILE_WIDTH;
+  const containerHeight = FIELD_HEIGHT * TILE_WIDTH;
   gameContainer = new PIXI.TilingSprite(
     resources.closed.texture,
     containerWidth,
@@ -173,13 +246,25 @@ function setup(loader, resources) {
 
   gameContainer.name = 'gameContainer';
 
-  // centerField([width / 2, height / 2]);
-
   textures = resources;
+
+  for (let i=0; i < FIELD_MINES; i++) {
+    const x = Math.floor(Math.random() * FIELD_WIDTH);
+    const y = Math.floor(Math.random() * FIELD_HEIGHT);
+
+    if(isCellAdded([x, y])) {
+      i--;
+    } else {
+      getCell([x, y]).placeMine();
+
+      for await (let neighbourCell of getNeighbours([x,y])) {
+        neighbourCell.addNeighbour();
+      }
+    }
+  }
 }
 
 var gameContainer;
-// var fieldContainer = new PIXI.Container();
 var clickHandler = new PIXI.Container();
 clickHandler.interactive = true;
 app.stage.addChild(clickHandler);
@@ -187,10 +272,9 @@ app.stage.addChild(clickHandler);
 function centerField([x, y]) {
 	let centerX = app.renderer.width/2;
 	let centerY = app.renderer.height/2;
-	let newX = Math.floor(-x*WIDTH + centerX);
-  let newY = Math.floor(-y*WIDTH + centerY);
+	let newX = Math.floor(-x*TILE_WIDTH + centerX);
+  let newY = Math.floor(-y*TILE_WIDTH + centerY);
 
-	// fieldContainer.position.set(newX,newY);
 	gameContainer.position.set(newX,newY);
 }
 
@@ -203,7 +287,7 @@ clickHandler
   .on('touchstart', onDragStart)
   .on('touchmove', onDragMove)
   .on('touchend', onDragEnd)
-  .on('touchendoutside', onDragEnd)
+  .on('touchendoutside', onDragEnd);
 
 function onDragStart(event) {
   this.dragging = true;
@@ -213,12 +297,19 @@ function onDragStart(event) {
   this.startPosition = {x : gameContainer.position.x, y : gameContainer.position.y};
 }
 
-function onDragEnd() {
+async function onDragEnd() {
   this.dragging = false;
+
   if(! this.hasDragged) {
-    // if the mousebutton didnt move, it means the user clicked
-    console.log('cursor', cursor);
-    click(cursor);
+    const isNeedReveal = !isCellAdded(cursor);
+
+    getCell(cursor).open();
+
+    if (isNeedReveal) {
+      for await (let neighbourCell of getNeighbours(cursor, true)) {
+        neighbourCell.open();
+      }
+    }
   }
 }
 
@@ -231,20 +322,39 @@ function onDragMove(event) {
     const gameContainer = this.getChildByName('gameContainer');
 
     gameContainer.position.set(x,y);
-    if(Math.pow(this.startPosition.x-x,2)+Math.pow(this.startPosition.y-y,2)>Math.pow(WIDTH,2)/9) {
+    if (Math.pow(this.startPosition.x - x, 2) + Math.pow(this.startPosition.y - y, 2) > Math.pow(TILE_WIDTH, 2) / 9) {
       this.hasDragged = true;
+
+      console.log('gameContainer.children', field, gameContainer.children);
+      let position = event.data.getLocalPosition(gameContainer);
+      let cursorCellX = Math.floor(position.x/TILE_WIDTH);
+      let cursorCellY = Math.floor(position.y/TILE_WIDTH);
+      console.log('cursorCellX',cursorCellX)
+      console.log('cursorCellY',cursorCellY)
+      Object.values(field).forEach(cell => {
+        if (Math.pow(cell?.cellX - cursorCellX, 2) + Math.pow(cell?.cellY - cursorCellY, 2) < Math.pow(LOAD_RADIUS, 2)) {
+          if (!cell?.parent) {
+            gameContainer.addChild(cell);
+          }
+        } else {
+          if (cell?.parent) {
+            gameContainer.removeChild(cell)
+          }
+        }
+      })
     }
+    
   }
 
   let position = event.data.getLocalPosition(gameContainer);
-  let x = Math.floor(position.x/WIDTH);
-  let y = Math.floor(position.y/WIDTH);
+  let x = Math.floor(position.x/TILE_WIDTH);
+  let y = Math.floor(position.y/TILE_WIDTH);
   cursor = [x,y];
 }
 
 function onRightClick(event){
   // Controls.flag();
-  console.log('cursor', cursor);
-  click(cursor, true);
+  // console.log('cursor', cursor);
+  getCell(cursor).markFlagged();
   // centerField(cursor);
 }
